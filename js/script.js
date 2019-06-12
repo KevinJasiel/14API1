040419d9a72ce6a0830553e503e437264beb2baa
KevinJasiel <jassielkevin9@gmail.com>
2019-06-12 11:41:56 -0600

    Imagenes HD

--- js/script.js
+++ js/script.js
@@ -2,33 +2,19 @@ var P = angular.module("P",[])
 
 P.controller("Pokemon", function($scope,$rootScope,$http){
 	
-		/*$scope.ListadoPokemon = [];
-		for(var z=1;z<100;z++)
-		$http({
-			metod: "GET",
-			url: "https://pokeapi.co/api/v2/pokemon/"+z
-		}).then(function Datos(x) {
-			$scope.ListadoPokemon.push(x);
-			console.log(x);
-		})*/
-
-
-		P.controller("Pokemon", function($scope,$rootScope,$http){
-	
 		$scope.ListadoPokemon = [];
-		for(var z=1;z<100;z++)
+		for(var z=1;z<200;z++)
 		$http({
 			metod: "GET",
 			url: "https://pokeapi.co/api/v2/pokemon/"+z
 		}).then(function Datos(x) {
 			$scope.ListadoPokemon.push(x);
 			console.log(x);
+			for(var a=1,a<100,a++)
+			if(a<9){
+				$Scope.ListadoPokemon[a].data.sprites.front_default="https://assets.pokemon.com/assets/cms2/img/pokedex/full/00"+a+".png"
+			}else if(a>9){
+				$Scope.ListadoPokemon[a].data.sprites.front_default="https://assets.pokemon.com/assets/cms2/img/pokedex/full/0"+a+".png"
+			}
 		})
-
-		$scope.ListadoPokemon = [];
-		for(var i=1;i<100;i++)
-			$http({
-				metod: "GET",
-				url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"+i
-			}).then(function )
 });
